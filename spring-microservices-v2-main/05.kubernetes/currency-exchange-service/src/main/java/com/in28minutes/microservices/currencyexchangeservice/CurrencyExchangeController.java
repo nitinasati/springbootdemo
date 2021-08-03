303c6c85cf141db493341e71b2626534c8c5ce2d
AsatiN <AsatiN@EFML-SPK1643.fleet.ad>
2021-08-03 10:36:36 -0400

    changing to 13

--- spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -38,7 +38,7 @@ public class CurrencyExchangeController {
 		
 		//CHANGE-KUBERNETES
 		String host = environment.getProperty("HOSTNAME");
-		String version = "v12";
+		String version = "v13";
 		
 		currencyExchange.setEnvironment(port + " " + version + " " + host);
 		
