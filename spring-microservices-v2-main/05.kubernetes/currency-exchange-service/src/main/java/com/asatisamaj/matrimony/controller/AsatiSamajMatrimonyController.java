2d4bca56187328cd8279455b44a92b856127a6f4
Asati <nasati@elementcorp.com>
2021-08-15 15:38:25 -0400

    adding pagination

--- spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/asatisamaj/matrimony/controller/AsatiSamajMatrimonyController.java
+++ spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/asatisamaj/matrimony/controller/AsatiSamajMatrimonyController.java
@@ -1,13 +1,22 @@
 package com.asatisamaj.matrimony.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asatisamaj.matrimony.entities.CurrencyExchange;
@@ -29,9 +38,36 @@ public class AsatiSamajMatrimonyController {
     @Autowired
     private Environment environment;
 
-    @GetMapping("/asatisamaj/matrimony/getMemberList")
+    @GetMapping("/asatisamaj/matrimony/getAllMemberList")
     public List<MemberDetails> getMemberList() {
         return memberRepository.findAll();
+    }
+
+    @GetMapping("/asatisamaj/matrimony/getMemberListByFilterPage")
+    public ResponseEntity<Map<String, Object>> getMemberListByFilterPage(@RequestParam(required = false) String education,
+            @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "3") int size) {
+
+        try {
+            List<MemberDetails> memberDetails;
+            Pageable paging = PageRequest.of(page, size);
+
+            Page<MemberDetails> pageTuts;
+            if (education == null)
+                pageTuts = memberRepository.findAll(paging);
+            else
+                pageTuts = memberRepository.findByEducationContaining(education, paging);
+
+            memberDetails = pageTuts.getContent();
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("MemberDetails", memberDetails);
+            response.put("currentPage", pageTuts.getNumber());
+            response.put("totalItems", pageTuts.getTotalElements());
+            response.put("totalPages", pageTuts.getTotalPages());
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
--- spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/asatisamaj/matrimony/repository/MemberDetailsRepository.java
+++ spring-microservices-v2-main/05.kubernetes/currency-exchange-service/src/main/java/com/asatisamaj/matrimony/repository/MemberDetailsRepository.java
@@ -1,9 +1,14 @@
 package com.asatisamaj.matrimony.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.asatisamaj.matrimony.entities.MemberDetails;
 
 public interface MemberDetailsRepository 
 	extends JpaRepository<MemberDetails, Long> {
+    
+    Page<MemberDetails> findBySamajArea(String samajArea, Pageable pageable);
+    Page<MemberDetails> findByEducationContaining(String title, Pageable pageable);
 }
